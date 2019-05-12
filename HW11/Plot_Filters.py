0319f6d86a3273861294a4fbf354b042c266ac92
jjnelson6 <jovan_nelson@brown.edu>
2019-05-12 14:41:10 -0500

    DSP data and figures

---
+++ HW11/Plot_Filters.py
@@ -0,0 +1,40 @@
+from pylab import *
+
+
+Filename = "data_5.txt"
+
+f= open (Filename, 'r')
+Data= f.readlines()
+
+Rawdata = []
+MAF= []
+IIR = []
+FIR= []
+index=[]
+
+
+for n in range (len(Data)):
+	Spdata= Data[n].split("     ")
+	FIR_data= Spdata[4].split('\n')
+	index.append(int(Spdata[0]))	
+	Rawdata.append(float(Spdata[1]))
+	MAF.append(float(Spdata[2]))
+	IIR.append(float(Spdata[3]))
+	FIR.append(float(FIR_data[0]))
+	
+
+figure()
+title('DSP: '+Filename)
+plot(index, Rawdata, 'ro-', label='Rawdata',)
+plot(index, MAF, 'g>-',label='MAF' )
+plot(index, IIR, 'bx-',label= 'IIR')
+plot(index, FIR, 'k<-',label= 'FIR')
+
+ylabel('Zaccl')
+xlabel('index')
+legend()
+
+
+show()
+
+
